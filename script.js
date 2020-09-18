6dad898f92c45c78db32df01ea15b3baa2815e3a
kiss107 <68926649+kiss107@users.noreply.github.com>
2020-09-18 11:34:27 -0300

    criarbackground

---
+++ script.js
@@ -0,0 +1,10 @@
+let canvas = document.getElementById("snake");
+let contex = canvas.getContex("2d");
+let box = 32;
+
+function criarBG() {
+    contex.fillStyle = "Lightgreen";
+    contex.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+criarBG();
